3f5c8cff72c4447d91f50ead69e4ff3043c1f3b2
Dell <1701214310@pku.edu.cn>
2020-04-11 17:36:20 +0800

    logical_not

--- src/jigsaw/tools.py
+++ src/jigsaw/tools.py
@@ -2,4 +2,9 @@ import torch
 
 
 def logical_not(tensor):
-    return 1-tensor if torch.__version__ == '1.2.0' else torch.logical_not(tensor)
+    if torch.__version__ == '1.1.0':
+        return 1-tensor
+    elif torch.__version__ == '1.2.0':
+        return ~tensor
+    else:
+        return torch.logical_not(tensor)
